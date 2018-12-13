7e68dc25bcbdb0b1fd427a4ce2aa9d5eb92f1d0b
Jared Moats <JaredMoats@outlook.com>
2018-12-13 15:11:21 -0500

    Checkpoint 2 - List Chat Rooms: Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 import logo from './logo.svg';
 import './App.css';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB7hPccuA7bKjOPs7cAVFfygxqljzyuLVw",
+  authDomain: "bloc-chat-react-jared-moats.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-jared-moats.firebaseio.com",
+  projectId: "bloc-chat-react-jared-moats",
+  storageBucket: "bloc-chat-react-jared-moats.appspot.com",
+  messagingSenderId: "292208506294"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList firebase={ firebase }/>
       </div>
     );
   }
