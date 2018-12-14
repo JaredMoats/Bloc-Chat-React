48d774af3a2792b2121d8771b07b52e860a50cdc
Jared Moats <JaredMoats@outlook.com>
2018-12-14 11:58:29 -0500

    Checkpoint 3 Create Chat Rooms: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,13 +6,19 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
 
     /* Object to interact with data in our database. */
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
+  /*
+    Sets this.state.rooms to the rooms already
+    in the database.
+    Executed when component is rendered.
+  */
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
@@ -22,16 +28,60 @@ class RoomList extends Component {
     });
   }
 
+  /*
+     Using state as the source of truth
+     for the value of input.
+     Update the state based on
+     what the user enters into
+     the input.
+  */
+  handleChange(event) {
+    this.setState({ newRoomName: event.target.value });
+  }
+
+  /*
+     Creates a new room and adds
+     it to the Firebase database.
+     Called by the form below.
+  */
+  createRoom(event) {
+    /* Prevent page refresh upon submission */
+    event.preventDefault();
+    /* Push the value in newRoomName to the firebase database */
+    const newRoom = this.state.newRoomName;
+    this.roomsRef.push({
+      name: newRoom
+    });
+    /* set this.state.newRoomName to an empty string */
+    const emptyString = '';
+    this.setState({ newRoomName: emptyString });
+  }
+
   render() {
     return(
       <nav className="container">
         <h1>Bloc Chat</h1>
         {
-          /* The first parameter is an object. Second is its index */
+          /*
+            Display the available Rooms on the page. 
+            The first parameter is an object. Second is its index.
+          */
           this.state.rooms.map((room, index) =>
             <div key={ index }><h3>{ room.name }</h3></div>
           )
         }
+        { /* This form creates a new room. */ }
+        <form onSubmit={ (event) => this.createRoom(event) }>
+          <label>
+            Create a New Room:
+          </label>
+          <input
+            type="text"
+            value={ this.state.newRoomName }
+            onChange={ (event) => this.handleChange(event) }
+          />
+          <input type="submit" value="Create Room" />
+        </form>
       </nav>
     );
   }
